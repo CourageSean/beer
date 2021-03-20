cca04353edb55d0d2aceff8066252b6017c9f035
CourageSean <seancourage23@gmail.com>
2021-03-20 23:41:35 +0100

    r

--- src/components/Detail.js
+++ src/components/Detail.js
@@ -1,18 +1,19 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Axios from "axios";
 
 function Detail(props) {
-  const [beerList, setBeerList] = useState("");
-  const call = () => {
+  const [beerInfo, setInfo] = useState("Jo");
+  useEffect(() => {
     Axios.get("https://ih-beers-api2.herokuapp.com/beers").then((response) => {
-      setBeerList(response);
+      const beerData = response.data;
+      setInfo(beerData);
+      console.log(beerData);
     });
-    console.log(beerList);
-  };
+  }, []);
 
   return (
     <div>
-      <h1>{}</h1>
+      <h1>{beerInfo[0].name}</h1>
     </div>
   );
 }
